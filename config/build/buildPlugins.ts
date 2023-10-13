67a3dc40ec7bcf71119c15e6617c3bccb059cf18
volovlikov <evgenyvolovlikov@mail.com>
2023-10-13 22:17:40 +0300

    add bundle analyzer & fix error

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions) {
@@ -17,5 +18,7 @@ export function buildPlugins({ paths, isDev }: BuildOptions) {
             __IS_DEV__: JSON.stringify(isDev),
         }),
         new webpack.HotModuleReplacementPlugin(),
+        // Анализ бандла файлов
+        // new BundleAnalyzerPlugin(),
     ];
 }
