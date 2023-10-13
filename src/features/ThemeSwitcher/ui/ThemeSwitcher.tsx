86bc28a7530f6deab8c35a3a9ccac08309c508b6
volovlikov <evgenyvolovlikov@mail.com>
2023-10-13 16:46:13 +0300

    add theme switcher & add file loader (assets)

---
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,28 @@
+import {memo} from 'react';
+import {classNames} from 'shared/classNames/classNames';
+import {useTheme} from 'shared/lib/hooks/useTheme/useTheme';
+import {Button, ThemeButton} from 'shared/ui/Button/Button';
+import {Theme} from 'shared/const/theme';
+import cls from './ThemeSwitcher.module.scss';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+
+
+interface ThemeSwitcherProps {
+    className?: string;
+}
+
+export const ThemeSwitcher = memo(({className}: ThemeSwitcherProps) => {
+    const {theme, toggleTheme} = useTheme();
+
+
+    return (
+        <Button
+            theme={ThemeButton.CLEAR}
+            className={classNames(cls.ThemeSwitcher, {}, [className])}
+            onClick={toggleTheme}
+        >
+            {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+        </Button> 
+    )
+})
