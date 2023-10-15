80e2b41ab53786971080f77196997e8e3054bdc2
volovlikov <evgenyvolovlikov@mail.com>
2023-10-15 12:43:23 +0300

    configure husky

--- src/shared/config/i18n/i18nForTests.ts
+++ src/shared/config/i18n/i18nForTests.ts
@@ -1,7 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-
 i18n.use(initReactI18next).init({
     lng: 'ru',
     fallbackLng: 'ru',
@@ -10,7 +9,7 @@ i18n.use(initReactI18next).init({
     interpolation: {
         escapeValue: false,
     },
-    resources: { ru: { translations: {} } }
-})
+    resources: { ru: { translations: {} } },
+});
 
 export default i18n;
