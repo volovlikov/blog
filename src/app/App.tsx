5aa2feef3e1f39654a4dc144c1f51299a9dc14a1
volovlikov <evgenyvolovlikov@mail.com>
2023-11-18 15:42:49 +0300

    add router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,13 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInited, userActions } from 'entities/User';
 
 function App() {
     const { theme } = useTheme();
     const dispatch = useDispatch();
+    const inited = useSelector(getUserInited);
 
     useEffect(() => {
         dispatch(userActions.initAuthData());
@@ -22,6 +23,7 @@ function App() {
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
+                    {inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
