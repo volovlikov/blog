c0fd9ce51dbc3638fe003a09aac0e97096640fd3
volovlikov <evgenyvolovlikov@mail.com>
2023-11-23 19:10:50 +0300

    add articles fetch data & integrate with client interface

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -3,6 +3,7 @@ export {
 } from './ui/ArticleDetails/ArticleDetails';
 export { ArticleList } from './ui/ArticleList/ArticleList';
 export { ArticleView } from './model/types/article';
+export { ArticleViewSelector } from './ui/ArticleViewSelector/ArticleViewSelector';
 
 export type { Article } from './model/types/article';
 export type { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
