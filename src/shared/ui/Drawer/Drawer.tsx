7207db3030f85c3c1add81935d552847deac203d
evolovlikov <evgenyvolovlikov@gmail.com>
2023-12-08 18:30:02 +0300

    add mobile detect for notification component

---
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -0,0 +1,41 @@
+import { useTheme } from 'app/providers/ThemeProvider';
+import { ReactNode, memo } from 'react';
+import { Mods, classNames } from 'shared/lib/classNames/classNames';
+import cls from './Drawer.module.scss';
+import { Portal } from '../Portal/Portal';
+import { Overlay } from '../Overlay/Overlay';
+
+interface DrawerProps {
+    className?: string;
+    children: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+}
+
+export const Drawer = memo((props: DrawerProps) => {
+    const {
+        children,
+        className,
+        isOpen,
+        onClose,
+    } = props;
+
+    const { theme } = useTheme();
+
+    const mods: Mods = {
+        [cls.opened]: isOpen,
+    };
+
+    return (
+        <Portal>
+            <div className={classNames(cls.Drawer, mods, [className, theme, 'app_drawer'])}>
+                <Overlay onClick={onClose} />
+                <div
+                    className={cls.content}
+                >
+                    {children}
+                </div>
+            </div>
+        </Portal>
+    );
+});
